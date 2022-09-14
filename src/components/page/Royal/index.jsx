b3976c95a543ed2521e196b287de541a2353641a
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-09-15 00:46:38 +0100

    Another one

--- src/components/page/Royal/index.jsx
+++ src/components/page/Royal/index.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import "./app.css";
 import { NavLink } from "../../atom/NavLink";
 import speeches from "../../../asset/speeches.png";
+import { Input } from "../../atom/Input";
 
 export const Royal = () => {
   return (
     <>
-      <div className="notice">
-        <div>
+      <div className="notice bc">
+        <div className="w-35">
         <img className="notice-img" src={speeches} alt="speeches" />
         </div>
-        <div className="notice-search">
+        <div className="notice-search w-30">
           <div className="">
-            <NavLink to="/" className="">
+            <NavLink to="/" className="backed">
              ⟵
             </NavLink>
-            <h1>Royal Court Notices</h1>
+            <h1 className="mb">Royal Court Notices</h1>
             <p className="fs-14">This is the office record of past royal engagements.</p>
+            <Input type="search" />
             </div>
         </div>
-        <div>c</div>
+        <div className="w-50 w h-5"></div>
       </div>
     </>
   );

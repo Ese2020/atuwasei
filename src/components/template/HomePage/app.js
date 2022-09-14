40434564f162ebd8f530f4cbfe600d4debcc4d7b
ovara1mark <markovara1@gmail.com>
2022-09-14 01:22:15 +0100

    new bottom nav

--- src/components/template/HomePage/app.js
+++ src/components/template/HomePage/app.js
@@ -15,11 +15,21 @@ export const Section = styled.section`
   text-align: center;
   font-size: 2.2rem;
   color: #ffffff;
-  padding-top: 19rem;
+  margin-top: 45vh;
 
   @media screen and (max-width: 998px) {
     font-size: 1.4rem;
-    padding-top: 22rem;
+    margin-top: 55vh;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  @media screen and (max-width: 668px) {
+    margin-top: 50vh;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  @media screen and (max-width: 50px) {
+    margin-top: 48vh;
     padding-left: 1rem;
     padding-right: 1rem;
   }
@@ -28,8 +38,8 @@ export const Section = styled.section`
 export const BottomNav = styled.footer`
   display: flex;
   justify-content: center;
-  padding-top: 5rem;
+  padding-top: 10vh;
   @media screen and (max-width: 998px) {
-    padding-top: 5rem;
+    padding-top: 12vh;
   }
 `;

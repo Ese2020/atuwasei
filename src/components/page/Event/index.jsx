c740739c1ca4cce6bb1a3e145e61e5e8c849af92
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-09-15 22:04:15 +0100

    Accept

--- src/components/page/Event/index.jsx
+++ src/components/page/Event/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./app.css";
+import { Navbar } from "../../molecule/Navbar";
 
 export const Event = () => {
   return (
     <>
       <div className="event-body">
+        <Navbar />
         <div className="nav-list">
-          <div className="nav-list">
+          <div className="gap-32">
             <span>Ceremonies</span>
             <span>•</span>
             <span>Events</span>
             <input placeholder="search" type="search" value=""></input>
           </div>
-          <span>See Blogs</span>
+          <span className="fs-24">See Blogs</span>
         </div>
       </div>
     </>

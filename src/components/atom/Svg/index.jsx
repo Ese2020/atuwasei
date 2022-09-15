f3be207c7ce6d20f72ec8bddecd68ad282344d58
ovara1mark <markovara1@gmail.com>
2022-09-15 09:34:07 +0100

    working navbar with pages

--- src/components/atom/Svg/index.jsx
+++ src/components/atom/Svg/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export const Svg = ({ src, alt }) => {
-  return <img src={src} alt={alt} />;
+export const Svg = ({ src, alt, className }) => {
+  return <img src={src} alt={alt} className={className} />;
 };

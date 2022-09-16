94eb97bd208daf74406a4e9f14947ce8a4d4f492
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-09-16 09:31:25 +0100

    Accept

--- src/components/page/Ginuwa/index.jsx
+++ src/components/page/Ginuwa/index.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./app.css";
+import { Input } from "../../atom/Input";
+import { NavLink } from "../../atom/NavLink";
 
 export const Ginuwa = () => {
   return (
     <>
-    <div>
+    <div className="gin-container bc">
       <div className="gin-img"></div>
-      <div></div>
+      <div className="w-60 br-30">
+        <div className="gin-info">
+          <div>
+          <NavLink to="/" className="back">
+             ⟵
+            </NavLink>
+            <h1 className="mb">The Ginuwa Ruling House</h1>
+            <p className="fs-14">Warri Kingdom.</p>
+            <Input type="search" />
+          </div>
+          <div>c
+          <div>a</div>
+          <div>b</div>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   );

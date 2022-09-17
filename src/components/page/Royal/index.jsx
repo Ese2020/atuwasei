9620859b1baf1babd44c63e82325cf01ed770a05
ovara1mark <markovara1@gmail.com>
2022-09-17 09:22:36 +0100

    new commit3e

--- src/components/page/Royal/index.jsx
+++ src/components/page/Royal/index.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import "./app.css";
 import { NavLink } from "../../atom/NavLink";
-import speeches from "../../../asset/speeches.png";
-import { Input } from "../../atom/Input";
+// import speeches from "../../../asset/speeches.png";
+// import { Input } from "../../atom/Input";
 
 export const Royal = () => {
   return (
     <>
       <div className="notice bc">
-        <div className="notice-img w-35">
-        </div>
+        <div className="notice-img w-35"></div>
         <div className="notice-search w-30">
           <div className="">
             <NavLink to="/" className="back">
-             ⟵
+              ⟵
             </NavLink>
             <h1 className="mb">Royal Court Notices</h1>
-            <p className="fs-14">This is the office record of past royal engagements.</p>
+            <p className="fs-14">
+              This is the office record of past royal engagements.
+            </p>
             <input type="text" placeholder="search" />
-            </div>
+          </div>
         </div>
         <div className="w-50 w h-5"></div>
       </div>

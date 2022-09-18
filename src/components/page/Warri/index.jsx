f7f3284a773cd7b0e1af4d6408ac854610734d88
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-09-18 21:59:01 +0100

    Update

--- src/components/page/Warri/index.jsx
+++ src/components/page/Warri/index.jsx
@@ -14,98 +14,98 @@ export const Warri = () => {
              ⟵
             </NavLink>
             <h1 className="mb t-20">The Warri Traditional Council</h1>
-            <p className="fs-14">Warri Kingdom.</p>
+            <p className="fs-14 mt-16">The Warri Traditional Council.</p>
             <input type="text" placeholder="search" />
           </div>
           <div className="warri-cards">
           <div className="w-cards">
-            <div className="w-card1 mb">
+            <div className="w-card mb">
               <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief Johnson Amatserunleghe</h5>
-              <p>The Iyatsere of Warri</p>
+              <p className=" mt-8">The Iyatsere of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card2 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
-              <h5>Chief Gabriel Awala</h5>
-              <p>The Uwangue of Warri</p>
+              <h5 className="mt-4">Chief Gabriel Awala</h5>
+              <p className="mt-8">The Uwangue of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card3 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief T.Y. Pessu</h5>
-              <p>The Ojomo of Warri</p>
+              <p className="mt-8">The Ojomo of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card4 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief Sunday Rone</h5>
-              <p> The Obazuaye of Warri</p>
+              <p className="mt-8"> The Obazuaye of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card5 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief (Dr.) P.E.B. Uku</h5>
-              <p>The Ere Oluyon of Warri</p>
+              <p className="mt-8">The Ere Oluyon of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card6 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief G.T. Grant</h5>
-              <p>The Agura-Otiri of Warri</p>
+              <p className="mt-8">The Agura-Otiri of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card7 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief (Mrs.) R. L. Ogbebor</h5>
-              <p>The Igba of Warri</p>
+              <p className="mt-8">The Igba of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card8 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief Kofi Kartey</h5>
-              <p>The Abolujiyan of Warri</p>
+              <p className="mt-8">The Abolujiyan of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card9 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief Brown Mene</h5>
-              <p>The Ogwaolusan of Warri</p>
+              <p className="mt-8">The Ogwaolusan of Warri</p>
               </div>
               </div>
             </div>
-            <div className="w-card10 mb">
+            <div className="w-card mb">
             <div className="w-card-logo"></div>
               <div className="w-card-details">
               <div>
               <h5>Chief Elder Walter Odeli (JP)</h5>
-              <p>The Oritsegbogwa of Warri</p>
+              <p className="mt-8">The Oritsegbogwa of Warri</p>
               </div>
               </div>
             </div>
@@ -113,13 +113,13 @@ export const Warri = () => {
           <hr className="break" />
           <div className="warri-descrip">
           <div className="w-card2-logo mt"></div>
-          <h3>Chief Johnson Amatserunleghe</h3>
+          <h3 className="mt-8">Chief Johnson Amatserunleghe</h3>
           <div className="more-details">
-            <p className="fs mb">
+            <p className="fs-16 mb mt-8">
               <b>Title:</b>
               The Iyatsere of Warri
             </p>
-            <p className="fs mb">
+            <p className="fs-16 mb">
               <b>Appellation:</b>
               Uji
             </p>

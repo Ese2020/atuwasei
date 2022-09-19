94d6c646c8ac39a13de8481ba0ec8735e29623de
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-09-19 08:45:10 +0100

    Update

--- src/components/organism/Gallery/index.jsx
+++ src/components/organism/Gallery/index.jsx
@@ -7,7 +7,8 @@ export const Gallery = () => {
         <>
         <div className="grid container">
             <div className="grid-info">
-                <div className="photo"></div>
+                <div className="photo">
+                </div>
                 <Text>Ògíamẹ̀'s Speech | Ògíamẹ̀ Atúwàtse III Economic Summit</Text>
             </div>
             <div className="grid-info">
